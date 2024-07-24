f258da4ccd877a14fd942092041053639abff0ed
0ctonaut <oct0naut@outlook.com>
2024-07-24 20:31:09 +0800

    convert to a premake5 project

--- hw2/global.hpp
+++ hw2/global.hpp
@@ -8,5 +8,6 @@
 //#define MY_PI 3.1415926
 //#define TWO_PI (2.0* MY_PI)
 
+#define SSAA_SAMPLE 2
 
 #endif //RASTERIZER_GLOBAL_H
--- hw2/rasterizer.hpp
+++ hw2/rasterizer.hpp
@@ -3,7 +3,6 @@
 //
 
 #pragma once
-#define SSAA_SAMPLE 8
 #include <eigen3/Eigen/Eigen>
 #include <algorithm>
 #include "global.hpp"
